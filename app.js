af2e01a7d9d23dc37d0a1d9f18237898908c3589
Andrii Hnatyshyn <andrii.hnatyshyn@gmail.com>
2018-08-08 20:56:20 +0300

    js-promise-async-await

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-let Car = require('./Car.js');
-let Truck = require('./Truck.js');
-
-
 console.log("\nNice work! Your first application have been launched.");
 console.log("Now, I need to know your name, type it below: ");
 
@@ -16,15 +12,63 @@ stdin.addListener("data", (txt) => {
         console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
         console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`);
 
-        let truck = new Truck('MAN','2000', 120);
-        let car = new Car('Toyota','1990', 220);
+        console.log('1');
+        console.log('2');
+        setTimeout(()=>{
+        console.log('3');
+        },3000);
+        console.log('4');
 
-        truck.transportContainer(); car.transportPeople();
+        console.log('1 step');
+        console.log('2 step');
+        //////////////PROMISES////////////////////
+        console.log('1');
+        console.log('2');
+        let p1 = new Promise((resolve,reject)=>
+        setTimeout(()=>{
+        console.log('3');
+        resolve("ready");
+        },3000));
+        
+        //p1.then(()=>{
+        // console.log('4');
+        //})
 
-        Car.showMaxSpeed();
+        async function test(promise){
+        let s1 = await promise;
+        //promise.then((result) => console.log("then result "+ result));
+        console.log("4");
+        //console.log(s1);
+        
+        return "I am function";
+        }
+        async function func(){
+        let f1 = await test(p1); console.log('----------------');
+        console.log(f1);
+        }
+        
+        func();
+        
+        ////////////////////////////////
 
-        truck.displayInfo();
-
-        car.carPrice();
+        function doubleAfter2Seconds(x) {
+            return new Promise(resolve => {
+              setTimeout(() => {
+                resolve(x * 2);
+              }, 2000);
+            });
+          }
+          
+          async function addAsync(x) {
+            const a = await doubleAfter2Seconds(10);
+            const b = await doubleAfter2Seconds(20);
+            const c = await doubleAfter2Seconds(30);
+            return x + a + b + c;
+          }
+          
+          
+          addAsync(10).then((sum) => {
+            console.log(sum);
+          });
     }
   });
