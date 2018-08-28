ee914bd350a6438d08bd1a2a6604b405c81d3936
Andrii Hnat <andrii.hnatyshyn@gmail.com>
2018-08-28 16:02:43 +0300

    js-protractor-p3:init

--- test_specs/login_spec.js
+++ test_specs/login_spec.js
@@ -1,6 +1,6 @@
 let WebButton = require("../controls/web.button");
-let WebButton = require("../controls/web.input");
-let WebButton = require("../controls/web.label");
+let WebTextInput = require("../controls/web.input");
+let WebTextView = require("../controls/web.label");
 
 describe('Login Functionality', () => {
 
@@ -10,7 +10,7 @@ describe('Login Functionality', () => {
     let loginField = new WebTextInput(element(by.id('email')), "Login field");
     let passwordField = new WebTextInput(element(by.id('userPassword')), "Password field");
 
-    let getlabel = new WebTextView(element(by.css(".user-name")),"Name");
+    let getlabel = new WebTextView(element(by.css(".user-name")),"Label Name");
 
     let logoutDrop = new WebButton(element(by.css('span.caret')), 'Logout Dropdown');
     let logoutButton = new WebButton(element(by.css('a.dropdown-item>strong')),"Logout Button");
@@ -31,40 +31,37 @@ describe('Login Functionality', () => {
         console.log("User Name - " + name );
     });
 
-    it('Also, ADMINISTRATOR should be able to login into eds_university.eleks.com', async () => {
-        await browser.get('http://eds_university.eleks.com/login');
+    // xit('Also, ADMINISTRATOR should be able to login into eds_university.eleks.com', async () => {
+    //     await browser.get('http://eds_university.eleks.com/login');
 
-        await loginButton.click();
+    //     await loginButton.click();
      
-        await loginField.sendKeys("andrii.hnatyshyn+scorer@gmail.com");
-        await passwordField.sendKeys("$FwQ5Yue");
+    //     await loginField.sendKeys("andrii.hnatyshyn+scorer@gmail.com");
+    //     await passwordField.sendKeys("$FwQ5Yue");
 
-        await signInButton.click();
+    //     await signInButton.click();
 
-        let name = await getlabel.getText();
+    //     let name = await getlabel.getText();
 
-        expect(name).toEqual('Andrii Hnatyshyn');
-        console.log("User Name - " + name );
-    });
+    //     expect(name).toEqual('Andrii Hnatyshyn');
+    //     console.log("User Name - " + name );
+    // });
 
-    it('User should be able to Logout from eds_university.eleks.com', async () => {
-        await browser.get('http://eds_university.eleks.com/login');
-        await loginButton.click();
-        await loginField.sendKeys("andrii.hnatyshyn+scorer@gmail.com");
-        await passwordField.sendKeys("$FwQ5Yue");
-        await signInButton.click();
+    // xit('User should be able to Logout from eds_university.eleks.com', async () => {
+    //     await browser.get('http://eds_university.eleks.com/login');
+    //     await loginButton.click();
+    //     await loginField.sendKeys("andrii.hnatyshyn+scorer@gmail.com");
+    //     await passwordField.sendKeys("$FwQ5Yue");
+    //     await signInButton.click();
 
-        let name = await getlabel.getText();
+    //     let name = await getlabel.getText();
            
-        await logoutDrop.click();
-        await logoutButton.click();
+    //     await logoutDrop.click();
+    //     await logoutButton.click();
 
-        let name = await element(by.css(".user-name")).getText();
+    //     let name = await element(by.css(".user-name")).getText();
         
-        expect(name).toEqual('Andrii Hnatyshyn');
-        console.log("User Name - " + name );
-    });
-
-    
-
+    //     expect(name).toEqual('Andrii Hnatyshyn');
+    //     console.log("User Name - " + name );
+    // });    
 });
